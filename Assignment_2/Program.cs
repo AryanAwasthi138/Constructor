c757090a6185d5ff6a1091ddce690e5d2eb63c92
AryanAwasthi138 <aryanawasthi138@gmail.com>
2024-07-11 16:58:57 +0530

    Done

---
+++ Assignment_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment_2
+{
+    public class Sport
+    {
+        public string type { get; set; }
+        public string equipments { get; set; }
+
+
+        public Sport()
+        {
+            Console.WriteLine("Default constructor is called on");
+        }
+        public Sport(string name)
+        {
+            Console.WriteLine("Parametrized constructor is called on");
+        }
+        public static void Main()
+        {
+            int parameter = 5;
+
+            Sport sport = new Sport();
+            Sport sport1 = new Sport("Parametrized Constructor");
+            sport.type = "Table Tennis";
+            sport.equipments = "Racquet,ball,table";
+
+
+
+
+        }
+    }
+}
